f4865d40d7487974ecae103f0f956947902490e9
nthangeniphumudzo <60381173+nthangeniphumudzo@users.noreply.github.com>
2022-10-02 21:32:34 +0200

    add time in progress'

---
+++ src/models/task.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+
+
+export interface ITask {
+    id?: String;
+    user?: String,
+    date?: String;
+    comment?: String;
+    projectId?: String;
+    time?: String;
+
+}
+
+
+252157539
+145207993
+249523254538420
