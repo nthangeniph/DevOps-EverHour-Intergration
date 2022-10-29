b98f7fb9a19134a2f1fd27a9aafb4f613693a399
Phumudzo Nthangeni <phumudzo.nthangeni@boxfusion.io>
2022-10-29 08:47:41 +0200

    deleting files

--- src/config/auth.config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const secret = "bezkoder-secret-key";
-exports.default = secret;
--- src/enums/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WorkItemTypes = void 0;
-const workItemType_1 = require("./workItemType");
-Object.defineProperty(exports, "WorkItemTypes", { enumerable: true, get: function () { return workItemType_1.WorkItemTypes; } });
